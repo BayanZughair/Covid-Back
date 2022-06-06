db86990de4744835ae2b4918c968e536f51d28e5
BayanZughair <106951236+BayanZughair@users.noreply.github.com>
2022-06-06 14:49:52 +0300

    .

--- Covid-Back/CovidAPI/settings.py
+++ Covid-Back/CovidAPI/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%zp(+14589rvve7_=jhm&jl4q_t^zs)n7p7=5wc9!&6zexcmzb'
+SECRET_KEY = 'django-insecure-(4gp6^)8d0+^d-*l0gm(r9tbic$gvf4=h)3jftdnno^q#p3tn9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
